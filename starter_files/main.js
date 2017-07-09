c19cc4f42bc41602f41b453e7b9b18258fd643dc
Tristan Schmitt <trschmitt50@live.com>
2017-07-09 16:44:03 -0400

    1-3 finished

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,4 +1,4 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
@@ -9,6 +9,13 @@ console.log(data);
 // 1: Show me how to calculate the average price of all items.
 function question1 () {
   // Answer:
+  let sum = 0 ;
+  let average = 0 ;
+  for (let i = 0; i < data.length; i++) {
+    sum += data[i].price;
+  }
+  average = sum / 25;
+  console.log("The average price is " + "$" + average + ".");
 }
 
 
@@ -16,6 +23,17 @@ function question1 () {
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
   // Answer:
+  let items = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].price >= 14 && data[i].price <= 18) {
+      items.push(data[i]);
+    }
+  }
+  console.log(items[0].title);
+  console.log(items[1].title);
+  console.log(items[2].title);
+  console.log(items[3].title);
+  console.log(items[4].title);
 }
 
 
@@ -23,16 +41,24 @@ function question2 () {
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
   // Answer:
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].currency_code = "USD") {
+      console.log(data[i].title + " costs " + data[i].price + " dollars.");
+    }
+  }
 }
 
 
 // 4: Display a list of all items who are made of wood.
 function question4 () {
   // Answer:
+  for (let i = 0; i < data.length; i++) {
+    data[i]
+  }
 }
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
